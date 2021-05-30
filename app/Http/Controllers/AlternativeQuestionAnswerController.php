5d49bedf3431dd0ac5ab72964baf59b8405f67d4
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-05-30 16:04:00 +0900

    php artisan make:model -a AlternativeQuestionAnswer

---
+++ app/Http/Controllers/AlternativeQuestionAnswerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AlternativeQuestionAnswer;
+use Illuminate\Http\Request;
+
+class AlternativeQuestionAnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AlternativeQuestionAnswer  $alternativeQuestionAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AlternativeQuestionAnswer $alternativeQuestionAnswer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AlternativeQuestionAnswer  $alternativeQuestionAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AlternativeQuestionAnswer $alternativeQuestionAnswer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AlternativeQuestionAnswer  $alternativeQuestionAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AlternativeQuestionAnswer $alternativeQuestionAnswer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AlternativeQuestionAnswer  $alternativeQuestionAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AlternativeQuestionAnswer $alternativeQuestionAnswer)
+    {
+        //
+    }
+}
