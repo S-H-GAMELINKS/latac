c36ac95e7e4f1d25ba4504dddbcb69d315877a40
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-05-30 15:57:31 +0900

    php artisan make:model AlternativeQuestion

---
+++ app/Http/Controllers/AlternativeQuestionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AlternativeQuestion;
+use Illuminate\Http\Request;
+
+class AlternativeQuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AlternativeQuestion  $alternativeQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AlternativeQuestion $alternativeQuestion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AlternativeQuestion  $alternativeQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AlternativeQuestion $alternativeQuestion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AlternativeQuestion  $alternativeQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AlternativeQuestion $alternativeQuestion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AlternativeQuestion  $alternativeQuestion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AlternativeQuestion $alternativeQuestion)
+    {
+        //
+    }
+}
---
+++ database/factories/AlternativeQuestionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AlternativeQuestion;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AlternativeQuestionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AlternativeQuestion::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
